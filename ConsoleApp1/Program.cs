258a75e42bd9c8cb2650ffe37b3e032c7734cad4
Sonu kumar kushwaha <51280276+flyingsonu122@users.noreply.github.com>
2020-08-01 12:54:38 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string myName = "Sonu kumar kushwaha";
+            int myAge = 19;
+            Console.WriteLine("Hello " + myName +  "!\nYou are " + myAge + " Years old.");
+            Console.WriteLine(myName.ToLower());
+            Console.WriteLine(myName.ToUpper());
+            Console.WriteLine(myName.Length);
+            Console.WriteLine(myAge + 10);
+            Console.WriteLine(5 * 2);
+            Console.WriteLine(5 % 2);
+
+            Console.ReadLine(); 
+        }
+    }
+}
